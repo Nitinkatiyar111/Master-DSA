36da728ca35b3cad58939fc58733335c7678964d
Nitinkatiyar111 <nkatiyar11@gmail.com>
2025-09-12 06:57:57 +0530

    Count Digits

---
+++ BasicMaths/CountDigits.java
@@ -0,0 +1,24 @@
+package BasicMaths;
+
+public class CountDigits {
+
+    public static int countDigit(int n){
+        int count = 0;
+        while (n > 0){
+            count++;
+            n = n / 10;
+        }
+
+            return count;
+    }
+
+    public static void main(String[] args){
+
+        int n = 101;
+
+       int result =  countDigit(n);
+
+       System.out.println(result);
+    }
+    
+}

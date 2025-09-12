4c02eddc2824daaf87066c105739b95be95fea61
Nitinkatiyar111 <nkatiyar11@gmail.com>
2025-09-12 12:02:08 +0530

    Factorial

---
+++ BasicMaths/Factorial.java
@@ -0,0 +1,25 @@
+package BasicMaths;
+
+public class Factorial {
+
+    public static int facto(int n){
+        int fact = 1;
+
+        for (int i = 1; i <= n; i++){
+            fact = fact * i;
+        }
+        return fact;
+
+    }
+
+    public static void main(String[] args){
+
+        int n = 5;
+
+        int result = facto(n);
+
+        System.out.println(result);
+
+    }
+    
+}

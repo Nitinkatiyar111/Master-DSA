bcdae90a6a84d945c86bef7ee19d642af0a67cf9
Nitinkatiyar111 <nkatiyar11@gmail.com>
2025-09-29 10:56:07 +0530

    Palindromcheck

---
+++ BasicRecursion/PalindromCheck.java
@@ -0,0 +1,30 @@
+package BasicRecursion;
+
+public class PalindromCheck {
+
+    public static boolean palindromCheck(String str){
+
+      return isPanlindromeCheck(str,0,str.length() - 1);
+    }
+
+     public static boolean isPanlindromeCheck(String str,int left,int right){
+
+            if (left >= right)
+            return true;
+
+            if (str.charAt(left) != str.charAt(right)){
+                return false;
+            }
+
+            return isPanlindromeCheck(str, left + 1, right - 1);
+
+     }
+
+    public static void main(String[] args){
+
+        String str = "nitin";
+
+        System.out.println(palindromCheck(str));
+    }
+    
+}

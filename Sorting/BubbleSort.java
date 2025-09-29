2983c45ad98fd795655171f156be5aec758a0799
Nitinkatiyar111 <nkatiyar11@gmail.com>
2025-09-29 13:04:39 +0530

    BubbleSort

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,37 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static int[] bubbleSort(int a[]){
+
+        int n = a.length;
+
+        // adjcent swap
+        for (int i = 0; i < n - 1; i++){
+            for (int j = 0; j < n - i - 1; j++){
+
+                if (a[j] > a[j+1]){
+                    int temp = a[j];
+                         a[j] = a[j+1];
+                         a[j+1] = temp;
+                }
+            }
+        }
+        return a;
+
+
+    }
+
+     public static void main(String[] args){
+
+        int a[] = {8,4,5,6,2,3};
+
+      int result[]  = bubbleSort(a);
+
+      System.out.print(Arrays.toString(bubbleSort(result)));
+    }
+    
+}
+    

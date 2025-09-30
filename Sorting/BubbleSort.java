6f51eef91d1f8f05cf77b75a5192b8e16e4abf9f
Nitinkatiyar111 <nkatiyar11@gmail.com>
2025-09-30 10:16:29 +0530

    BubbleSort

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -8,19 +8,18 @@ public class BubbleSort {
 
         int n = a.length;
 
-        // adjcent swap
-        for (int i = 0; i < n - 1; i++){
-            for (int j = 0; j < n - i - 1; j++){
+        for (int i = 0 ; i < n ; i++){
+            for (int j = 0 ; j < n - i - 1; j++){
 
-                if (a[j] > a[j+1]){
+                if (a[j] > a[ j + 1]){
                     int temp = a[j];
-                         a[j] = a[j+1];
-                         a[j+1] = temp;
+                    a[j] = a[j+1];
+                    a[j + 1] = temp;
                 }
             }
         }
+      
         return a;
-
 
     }
 

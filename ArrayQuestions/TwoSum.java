f11a001a504154f915269544c7da24f53cb107d1
Nitinkatiyar111 <nkatiyar11@gmail.com>
2025-10-12 01:33:58 +0530

    Two Sum

--- ArrayQuestions/TwoSum.java
+++ ArrayQuestions/TwoSum.java
@@ -12,7 +12,7 @@ public class TwoSum {
 
             int n = nums.length;
 
-            for (int i = 0; i < nums.length;i++){
+            for (int i = 0; i < n;i++){
                 
                 int num = nums[i];
                 
